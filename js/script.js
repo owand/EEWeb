4a2860c5d885cf9ec7325b8c19f6dd467e008592
Andrey Ovcharenko <plowand@gmail.com>
2021-12-08 15:08:15 +0200

    xxx

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 
-function toggleMenu() {
-   var menuCheckbox = document.getElementById('menu-check');
-   menuCheckbox.click();
-}
+var menuCheckbox = document.getElementById('menu-check');
+var menuItems = document.querySelectorAll('.category-list li');
+
+// close hamburger menu after click a
+for (var i = 0; i < menuItems.length; ++i) {
+   menuItems[i].addEventListener('click', function (e) {
+      menuCheckbox.click();
+   })
+};
